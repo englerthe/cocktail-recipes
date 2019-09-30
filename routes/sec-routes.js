9990a7e0f4cc9a53da2741ce4c9f12115b7118c7
Englert <englert@media-saturn.com>
2019-09-30 17:35:51 +0200

    adjustment in sec-routes.js, fixing add-receipes.hbs and seeds.js

--- routes/sec-routes.js
+++ routes/sec-routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Room = require("../models/room-model");
+const Recipes = require("../models/recipes-model.js");
 
 router.get("/", (req, res, next) => {
   res.render("index");
@@ -15,41 +15,50 @@ router.use((req, res, next) => {
 }); // ------------------------------------                                
 //     | 
 //     V
-router.get("/rooms", (req, res, next) => {
-  res.render("restricted/rooms");
+router.get("/recipes", (req, res, next) => {
+  res.render("restricted/add-recipes");
 });
 
-router.post("/rooms/add", (req, res, next) => {
-  const name = req.body.name;
+router.post("/recipes/create-recipes", (req, res, next) => {
+  const title = req.body.title;
+  const rating = req.body.rating;
+  const servings = req.body.servings;
+  const ingredients = req.body.ingredients;
   const description = req.body.description;
   const imageUrl = req.body.imageUrl;
   const owner = req.session.currentUser._id;
+  const reviews = req.body.reviews;
+  console.log(title, rating, servings, ingredients, description, imageUrl, owner, reviews);
   
-  if (name === "") { //check if post values are not empty
-    res.render("restricted/rooms", { // create signup error message in signup page
+  if (title === "") { //check if post values are not empty
+    res.render("restricted/add-recipes", { // create signup error message in signup page
       Message: "Enter all necessary data."
     });
     return;
   }
-  Room.findOne({ "name": name }) // check if room exists
-    .then(thisRoom => {
-      if (thisRoom !== null) {
-        res.render("restricted/rooms", { // create signup error message in signup page
-          Message: "The room " + name + " already exists!"
+  Recipes.findOne({ "name": title }) // check if recipe exists
+    .then(thisRecipes => {
+      if (thisRecipes !== null) {
+        res.render("restricted/add-recipes", { // create signup error message in signup page
+          Message: "The recipe " + title + " already exists!"
         });
         return;
       }
-      Room.create({
-        name: name,
-        description: description,
-        imageUrl: imageUrl,
-        owner: owner
+    Recipes.create({
+      title: title,
+      rating: rating,
+      servings: servings,
+      ingredients: ingredients,
+      description: description,
+      imageUrl: imageUrl,
+      owner: owner,
+      reviews: reviews
       })
         .then(() => {
-          res.render("restricted/rooms", { Message: "The room " + name + " has been created successfully!" });
+          res.render("restricted/add-recipes", { Message: "The recipe " + title + " has been created successfully!" });
         })
         .catch(error => {
-          console.log("Create room failed: " + error);
+          console.log("Create recipe failed: " + error);
         })
     })
     .catch(error => {
@@ -57,31 +66,33 @@ router.post("/rooms/add", (req, res, next) => {
     })
 });
 
-router.post("/rooms/edit", (req, res, next) => {
-  const name = req.body.name;
+router.post("/recipes/edit", (req, res, next) => {
+  const title = req.body.title;
+  const rating = req.body.rating;
+  const servings = req.body.servings;
+  const ingredients = req.body.ingredients;
   const description = req.body.description;
   const imageUrl = req.body.imageUrl;
+  const reviews = req.body.reviews;
 
-  if (name === "") { //check if post values are not empty
-    res.render("restricted/rooms", {
+  if (title === "") { //check if post values are not empty
+    res.render("restricted/add-recipes", {
       Message: "Enter all necessary data."
     });
     return;
   }
-  Room.findOne({ name: name })
-    .then(thisRoom => {
-      //console.log(req.session.currentUser._id, thisRoom.owner);
-      //if (req.session.currentUser._id == thisRoom.owner) {
-      if (req.session.currentUser._id.equals(thisRoom.owner)) {
-      Room.update({ _id: thisRoom._id }, { $set: { name, description, imageUrl } })
+  Recipes.findOne({ title: title })
+    .then(thisRecipes => {
+      if (req.session.currentUser._id.equals(thisRecipes.owner)) {
+        Recipes.update({ _id: thisRecipes._id }, { $set: { title, rating, servings, ingredients, description, imageUrl, reviews } })
         .then(() => {
-          res.render("restricted/rooms", { Message: "The room " + name + " has been edited successfully!" })
+          res.render("restricted/add-recipes", { Message: "The recipe " + title + " has been edited successfully!" })
         })
         .catch(error => {
           next(error);
         })
       } else {
-        res.render("restricted/rooms", { Message: "The room " + name + " has not been edited!" })
+        res.render("restricted/add-recipes", { Message: "The recipe " + title + " has not been edited!" })
       }  // end if
     })
     .catch(error => {
@@ -89,28 +100,27 @@ router.post("/rooms/edit", (req, res, next) => {
     })
   });
 
-  router.post("/rooms/delete", (req, res, next) => {
-    const name = req.body.name;
+  router.post("/recipes/delete", (req, res, next) => {
+    const title = req.body.title;
   
-    if (name === "") { //check if post values are not empty
-      res.render("restricted/rooms", {
+    if (title === "") { //check if post values are not empty
+      res.render("restricted/add-recipes", {
         Message: "Enter all necessary data."
       });
       return;
     }
-    Room.findOne({ name: name })
-      .then(thisRoom => {
-        //console.log(req.session.currentUser._id, thisRoom.owner);
-        if (req.session.currentUser._id == thisRoom.owner) {
-        Room.findByIdAndDelete(thisRoom._id)
+    Recipes.findOne({ title: title })
+      .then(thisRecipes => {
+        if (req.session.currentUser._id.equals(thisRecipes.owner)) {
+          Recipes.findByIdAndDelete(thisRecipes._id)
           .then(() => {
-            res.render("restricted/rooms", { Message: "The room " + name + " has been deleted successfully!" })
+            res.render("restricted/add-recipes", { Message: "The recipe " + title + " has been deleted successfully!" })
           })
           .catch(error => {
             next(error);
           })
         } else {
-          res.render("restricted/rooms", { Message: "The room " + name + " has not been deleted!" })
+          res.render("restricted/add-recipes", { Message: "The recipe " + title + " has not been deleted!" })
         }  // end if
       })
       .catch(error => {
